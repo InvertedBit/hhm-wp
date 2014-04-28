e4dd823daee4079eed386d4c96795b53c9ce10f6
Alex Scherer <alex.scherer@outlook.com>
2014-04-28 09:11:47 -0700

    initial commit

---
+++ hhm-wp.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Holiday Home Manager WP-Plugin
+ * Plugin URI: http://hhm.dev.local/
+ * Description: Wordpress Plugin for the Holiday Home Manager
+ * Version: 0.01.00
+ * Author: Alex Scherer
+ * Author URI: http://alexscherer.ch/
+ * License: Proprietary
+ */
